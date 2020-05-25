2f487a1aca28a005a69ad5bccd6d87f54aad88cd
Klishin Ilya <klishin-ilya@mail.ru>
2020-05-25 22:52:21 +0300

    app module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material';
+//user
+import { ComponentsModule } from "./components/components.module";
+import { LayoutsModule } from './layouts/layouts.module';
+import { PagesModule } from './pages/pages.module';
+import { AppConstants } from './app.constants';
+import { AuthService } from './services/auth/auth.service';
+import { DialogsService } from './services/ui/dialogs.service';
+import { AnalysesService } from './services/analyses/analyses.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule,
+    ComponentsModule,
+    LayoutsModule,
+    PagesModule,
   ],
-  providers: [],
+  providers: [
+    AppConstants,
+    AuthService,
+    DialogsService,
+    AnalysesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
