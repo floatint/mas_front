bb7432115ab427c277f7cd64a6c66d1a66b60d02
Klishin Ilya <klishin-ilya@mail.ru>
2020-05-25 22:51:38 +0300

    constants module update

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -5,4 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class AppConstants {
     public ORDER_ANALYSIS_ID = 'order_analysis_id';
+    public USER_TOKEN = 'user_token';
+    public USER_MODEL = 'user_model';
 }
